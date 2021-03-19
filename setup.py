5ad46611beb68b2a38ede2263b0d7d2b4e7bef0f
MrbeastmodeXD <70970191+MrbeastmodeXD@users.noreply.github.com>
2021-03-18 23:11:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'Simple package to help parse PDP on Footsites!'
 LONGDESCRIPTION = "Check https://github.com/MrbeastmodeXD/PDP-Parser for documentation! : D\n"
 # Setting up
